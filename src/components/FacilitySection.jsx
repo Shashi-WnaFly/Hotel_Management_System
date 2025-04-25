7b3ca5b67913e2101ed8f069f4b780b21cdd4b44
Shashi-Anand <shashianand2600@gmail.com>
2025-04-25 20:21:18 +0530

    fixed map key error

--- src/components/FacilitySection.jsx
+++ src/components/FacilitySection.jsx
@@ -3,21 +3,25 @@ import FacilityCard from "./FacilityCard";
 
 const cardDetails = [
   [
+    "12kijnfedra34",
     "https://www.clipartmax.com/png/middle/49-495395_dorms-hotel-room-icon-png.png",
     "Rooms and Suites",
     "Varied types of rooms, from standard to luxury suites, equipped with essentials like beds.",
   ],
   [
+    "12dnhdaqw35",
     "https://t4.ftcdn.net/jpg/05/62/77/49/360_F_562774997_ddVwQB07kXKV2o0QrpszXL2x7d9jM8Wd.jpg",
     "24-Hour Security",
     "On-site security personnel and best surveillance. from standard to luxury, Secure storage.",
   ],
   [
+    "12haehjghf36",
     "https://img.favpng.com/14/16/15/physical-fitness-fitness-centre-physical-exercise-computer-icons-png-favpng-wKuvJCsvhDeVcNJriKWci6zka.jpg",
     "Fitness Center",
     "Equipped with exercise machines and weights.Offering massages, facials, and other.",
   ],
   [
+    "12shsnsdsa37",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJlJ3zho_u6JSOLQlJFAkxB0QVTeKvDmv_yG8yMMFVgw&s&ec=72940545",
     "Swimming Pool",
     "Indoor or outdoor pools for leisure or exercise.Offering massages, facials, and other",
@@ -32,9 +36,10 @@ const FacilitySection = () => {
         <div className="flex gap-4 flex-wrap justify-evenly">
           {cardDetails.map((details) => (
             <FacilityCard
-              url={details[0]}
-              title={details[1]}
-              description={details[2]}
+              key={details[0]}
+              url={details[1]}
+              title={details[2]}
+              description={details[3]}
             />
           ))}
         </div>
